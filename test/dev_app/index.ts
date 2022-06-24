fdb27f77ccc0142fd24b40142e144564d6fbedad
AlexxNB <iam@alexxnb.ru>
2022-06-24 09:48:23 +0300

    Full dev sequence

--- test/dev_app/index.ts
+++ test/dev_app/index.ts
@@ -11,16 +11,47 @@ if(!process.env.TELEGRAM_BOT_TOKEN)
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN,{polling:true});
 const dialogs = new Dialogs(bot,{
-  locale: "ru"
+  locale: "en"
 });
 
 const testDialog = dialogs.create([
   {
+    type:"text",
+    message:"What is your name?",
+    name: 'name'
+  },
+  {
+    type:"datepicker",
+    message:"When you born?",
+    name: 'birthday'
+  },
+  {
+    type:"confirm",
+    message:"Are you shure?",
+    name: 'shure'
+  },
+  {
+    type:"pinpad",
+    message:"What is your phone number?",
+    name: 'phone',
+    mask: "+7 (***) ***-**-**"
+  },
+  {
     type:"select",
-    message:"Test select question.",
-    name: 'test_1',
-    buttons: [[{a:"A"},{b:"B"},{c:"C"}]],
-    multiple: false
+    name: "sport",
+    message: "What are your favourite sports?",
+    buttons:[
+      ["Soccer"],
+      ["Hockey"],
+      ["Tennis","Squash"],
+    ],
+    multiple: true
+  },
+  {
+    type: "picker",
+    name: "color",
+    message: "Choose yours most loved color.",
+    list:["Pink","Orange","Red","Blue","Green"]
   }
 ]);
 
